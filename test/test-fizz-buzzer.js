61848783a233c6f48fc75f38aae94cddd4f6dcff
Casey Whitcher <sandiegocasey@gmail.com>
2018-06-27 12:16:25 -0700

    updating fizzbuzz test

---
+++ test/test-fizz-buzzer.js
@@ -0,0 +1,40 @@
+// import chai, declare expect variable
+const expect = require('chai').expect;
+
+// import adder
+const fizzer = require('../fizzBuzzer');
+
+// unit tests for our `adder` function
+describe('fizzBuzzer', function() {
+
+  // test the normal case
+  it('should return fizz-buzz', function() {
+    // range of normal inputs, including
+    // notable cases like negative answers
+    const normalCases = [
+      {num: 15, expected: 'fizz-buzz'},
+      {num: 5, expected: 'buzz'},
+      {num: 3, expected: 'fizz'}
+    ];
+    // for each set of inputs (a, b), `adder` should
+    // produce the expected value
+    normalCases.forEach(function(input) {
+      const answer = fizzer(input.num);
+      expect(answer).to.equal(input.expected);
+    });
+  });
+
+  it('should raise error if args not numbers', function() {
+    // range of bad inputs where not both are numbers
+    const badInputs = [
+      ['a'],
+      [false]
+    ];
+    // prove that an error is raised for bad inputs
+    badInputs.forEach(function(input) {
+      expect(function() {
+        fizzer(input[0], input[1]);
+      }).to.throw(Error);
+    });
+  });
+});
